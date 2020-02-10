f62bdceb38d120e2ae5955108cb532b934ab1e53
kondasMajid <kondasmajid@gmail.com>
2020-02-10 14:20:47 -0800

    contentx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AngularFireModule } from 'angularfire2/';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-// import { SimpleComponent } from './simple.component';
-import { FormsModule } from '@angular/forms';
 import { SimpleComponent } from './simple/simple.component';
-// import { FormsModule } from '@angular/forms';
 
-export const environment  = {
-   firebaseConfig : {
-    apiKey: "AIzaSyDMnGH7Yd8ur9eKYlDtcibugkAwv4-HuF8",
-    authDomain: "angu-chat.firebaseapp.com",
-    databaseURL: "https://angu-chat.firebaseio.com",
-    projectId: "angu-chat",
-    storageBucket: "angu-chat.appspot.com",
-    messagingSenderId: "50909560705",
-    appId: "1:50909560705:web:64e8c214ab248993793280"
-  }
-}
+
 
 @NgModule({
   declarations: [
@@ -28,8 +13,6 @@ export const environment  = {
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireDatabaseModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
